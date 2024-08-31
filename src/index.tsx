e8d69b88e4e95af68b8d2ca36e0c6170d8bd32e8
Horacio Daniel Ros <horaciodrs@gmail.com>
2024-08-31 00:25:52 -0300

    add theme support

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import "./style.css";
 
+import { createApplication } from "@cedro/core/application.builder";
 import { Application, Routes, Route, Widgets } from "@cedro/core/application.core";
 import { WContainer } from "@cedro/ui/container.ui";
-import { createApplication } from "@cedro/core/application.builder";
 
 window.app = (() => {
+    const onLoadHandler = () => {
+        const router = app?.router;
+        const routeData = router?.getCurrentLocation().params;
+
+        if (routeData?.theme && routeData?.theme !== "cedro") {
+            app?.theme.setTheme(routeData?.theme);
+        }
+    };
+
     return createApplication(
         <Application
             title="Ceddro | Examples"
             padding={0}
             orientation="vertical"
-            theme="dark"
-            onLoad={() => {}}
+            theme="cedro"
+            onLoad={onLoadHandler}
         >
             <Widgets>
                 <WContainer id="main-container" orientation="vertical"></WContainer>
